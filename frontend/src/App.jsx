f671c9f139cd12b8fc3dea719c297748d439e51c
Manas <123manassalunke@gmail.com>
2024-02-28 17:50:52 +0530

    useEffect added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import { CreateTodo } from './components/CreateTodo'
 import { Todos } from './components/Todos'
@@ -7,11 +7,14 @@ function App() {
   const [todos, setTodos] = useState([]);// we have to give an initial value
 
   // with useEffect send a fetch call to backend
-  fetch("http://localhost:3000/todos")
+  useEffect(()=>{
+    fetch("http://localhost:3000/todos")
   .then(async function(res){
     const json = await res.json();
     setTodos(json.todos)
   })
+  },[])
+  
 
   return (
     <>
